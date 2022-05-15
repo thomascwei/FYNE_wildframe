b8a6cccf8637e50d7b5a3804ca76769fe748c9c0
thomascwei <thomas.c.wei@gmail.com>
2022-05-15 18:51:46 +0800

    .

--- 37_file_choose_and_select_entry/main.go
+++ 37_file_choose_and_select_entry/main.go
@@ -7,6 +7,7 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/jasonlvhit/gocron"
 	"sync/atomic"
@@ -16,9 +17,9 @@ var SendCount int32 = 0
 
 func taskWithParams(Label4 *widget.Label, File, SelectEntry string) {
 	atomic.AddInt32(&SendCount, 1)
+	fmt.Printf("I am running %s %s. Send count : %d.\n", File, SelectEntry, SendCount)
 	if SendCount%10 == 0 {
-		fmt.Printf("I am running %s %s. Send count to %d.\n", File, SelectEntry, SendCount)
-		Label4.Text = fmt.Sprintf("Send count to : %d", SendCount)
+		Label4.Text = fmt.Sprintf("Send count : %d", SendCount)
 		Label4.Refresh()
 	}
 }
@@ -30,18 +31,15 @@ func main() {
 	// New app
 	a := app.New()
 	// new title and window
-	w := a.NewWindow("Select entry widget, drop down")
+	w := a.NewWindow("VAST stress test LAS60X")
 	// resize window
 	w.Resize(fyne.NewSize(400, 400))
 	// lets show our selected entry in label
 	label1 := widget.NewLabel("data type : ")
 	label2 := widget.NewLabel("file : ")
-	label3 := widget.NewLabel("ready???")
-	label4 := widget.NewLabel("Count to:")
+	label3 := widget.NewLabel("")
+	label4 := widget.NewLabel("Send count : 0")
 
-	// dropdown/ select entry
-	//[]string{} all our option goes in slice
-	// s is the variable to get the selected value
 	dd := widget.NewSelect(
 		[]string{"temperature", "battery_usage"},
 		func(s string) {
@@ -51,7 +49,7 @@ func main() {
 		})
 	// more than one widget. so use container
 
-	btn1 := widget.NewButton("Click to choose a file", func() {
+	btn1 := widget.NewButton("click to choose a file", func() {
 		dialog.ShowFileOpen(
 			func(r fyne.URIReadCloser, _ error) {
 				fmt.Println(r.URI().Path())
@@ -63,33 +61,51 @@ func main() {
 				label2.Refresh()
 			}, w)
 	})
+	btn1.Resize(fyne.NewSize(200, 30))
+	btn1.Move(fyne.NewPos(40, 5))
 
-	btn2 := widget.NewButton("start", func() {
-		label3.Text = " GO!!! " + label1.Text + " " + label2.Text
-		label3.Refresh()
-		// Clear all scheduled jobs
-		label4.Text = fmt.Sprintf("Send count to : 0")
-		label4.Refresh()
-		err := gocron.Every(1).Second().Do(taskWithParams, label4, FilePath, DataType)
-		if err != nil {
-			panic(err)
-		}
-		// Start all the pending jobs
-		go func() {
-			<-gocron.Start()
-		}()
+	toolbar := widget.NewToolbar(
+		widget.NewToolbarSpacer(),
+		widget.NewToolbarAction(theme.MediaPlayIcon(), func() {
+			if DataType == "" {
+				label3.Text = "DataType Error"
+				label3.Refresh()
+			} else if FilePath == "" {
+				label3.Text = "File Error"
+				label3.Refresh()
+			} else {
+				label3.Text = "GO!!! " + label1.Text + " " + label2.Text
+				label3.Refresh()
+				// Clear all scheduled jobs
+				label4.Text = fmt.Sprintf("Send count : 0")
+				label4.Refresh()
+				gocron.Clear()
+				atomic.StoreInt32(&SendCount, 0)
+				err := gocron.Every(1).Second().Do(taskWithParams, label4, FilePath, DataType)
+				if err != nil {
+					panic(err)
+				}
+				// Start all the pending jobs
+				go func() {
+					<-gocron.Start()
+				}()
+			}
+		}),
+		//widget.NewToolbarSpacer(),
+		widget.NewToolbarAction(theme.MediaStopIcon(), func() {
+			label3.Text = "STOP!!!"
+			label3.Refresh()
+			fmt.Println(label3.Text)
+			gocron.Clear()
+			atomic.StoreInt32(&SendCount, 0)
+		}),
+		widget.NewToolbarSpacer(),
+	)
 
-	})
-
-	btn3 := widget.NewButton("stop", func() {
-		fmt.Println("clear")
-		gocron.Clear()
-		atomic.StoreInt32(&SendCount, 0)
-
-	})
-
-	c := container.NewVBox(dd, label1, btn1, label2, btn2, label3, label4, btn3)
+	btn11 := container.NewWithoutLayout(btn1)
+	c := container.NewVBox(dd, label1, btn11, label2, label3, label4, toolbar)
 	w.SetContent(c)
+
 	//show and run
 	w.ShowAndRun()
 }
