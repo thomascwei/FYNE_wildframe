d40f8bcc8ea5209fe03afbc0824b1162e9c7cf7d
thomascwei <thomas.c.wei@gmail.com>
2022-05-15 13:13:40 +0800

    .

--- 37_file_choose_and_select_entry/main.go
+++ 37_file_choose_and_select_entry/main.go
@@ -8,11 +8,25 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
+	"github.com/jasonlvhit/gocron"
+	"sync/atomic"
 )
+
+var SendCount int32 = 0
+
+func taskWithParams(Label4 *widget.Label, File, SelectEntry string) {
+	atomic.AddInt32(&SendCount, 1)
+	if SendCount%10 == 0 {
+		fmt.Printf("I am running %s %s. Send count to %d.\n", File, SelectEntry, SendCount)
+		Label4.Text = fmt.Sprintf("Send count to : %d", SendCount)
+		Label4.Refresh()
+	}
+}
 
 func main() {
 	var DataType string
 	var FilePath string
+
 	// New app
 	a := app.New()
 	// new title and window
@@ -23,6 +37,7 @@ func main() {
 	label1 := widget.NewLabel("data type : ")
 	label2 := widget.NewLabel("file : ")
 	label3 := widget.NewLabel("ready???")
+	label4 := widget.NewLabel("Count to:")
 
 	// dropdown/ select entry
 	//[]string{} all our option goes in slice
@@ -36,7 +51,7 @@ func main() {
 		})
 	// more than one widget. so use container
 
-	btn1 := widget.NewButton("Open a file", func() {
+	btn1 := widget.NewButton("Click to choose a file", func() {
 		dialog.ShowFileOpen(
 			func(r fyne.URIReadCloser, _ error) {
 				fmt.Println(r.URI().Path())
@@ -49,12 +64,31 @@ func main() {
 			}, w)
 	})
 
-	btn2 := widget.NewButton("execute", func() {
+	btn2 := widget.NewButton("start", func() {
 		label3.Text = " GO!!! " + label1.Text + " " + label2.Text
 		label3.Refresh()
+		// Clear all scheduled jobs
+		label4.Text = fmt.Sprintf("Send count to : 0")
+		label4.Refresh()
+		err := gocron.Every(1).Second().Do(taskWithParams, label4, FilePath, DataType)
+		if err != nil {
+			panic(err)
+		}
+		// Start all the pending jobs
+		go func() {
+			<-gocron.Start()
+		}()
+
 	})
 
-	c := container.NewVBox(dd, label1, btn1, label2, btn2, label3)
+	btn3 := widget.NewButton("stop", func() {
+		fmt.Println("clear")
+		gocron.Clear()
+		atomic.StoreInt32(&SendCount, 0)
+
+	})
+
+	c := container.NewVBox(dd, label1, btn1, label2, btn2, label3, label4, btn3)
 	w.SetContent(c)
 	//show and run
 	w.ShowAndRun()
